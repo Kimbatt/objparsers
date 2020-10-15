e3ea41332b7a43570118949e850427b4725b380f
mark-leopoly <mark.kovacs@leopoly.com>
2020-10-15 16:28:19 +0200

    test with more models

--- objparser/src/test-app/main.rs
+++ objparser/src/test-app/main.rs
@@ -1,19 +1,30 @@
 
 fn main() -> Result<(), Box<dyn std::error::Error>>
 {
-    let start = std::time::Instant::now();
+    for file in  std::fs::read_dir("res/testmodels/common-3d-test-models/")?
+    {
+        let file_path = file?.path();
+        let file_path_str = file_path.to_str().ok_or("Path error")?;
+        let start = std::time::Instant::now();
+    
+        let result = objparser::obj::obj::load_obj(file_path_str,
+            objparser::obj::obj::ObjParseFeatures::NONE)?;
+    
+        let end = std::time::Instant::now();
+        println!("done in {}ms", end.duration_since(start).as_millis());
+    
+        println!("vertices: {}", &result.positions.len());
+        println!("texcoords: {}", if let Some(texcoords) = &result.texcoords { texcoords.len() } else { 0 });
+        println!("normals: {}", if let Some(normals) = &result.normals { normals.len() } else { 0 });
+        println!("indices: {}", &result.indices.len());
+    
+        result.export(format!("res/testexport/{}",
+            std::path::Path::new(file_path_str).file_name()
+            .ok_or("Path error")?
+            .to_str()
+            .ok_or("Path error")?).as_str())?;
+    }
 
-    let result = objparser::obj::obj::load_obj("res/WoodenToyShapelab.obj", objparser::obj::obj::ObjParseFeatures::LOAD_VERTEX_NORMALS)?;
-
-    let end = std::time::Instant::now();
-    println!("done in {}ms", end.duration_since(start).as_millis());
-
-    println!("vertices: {}", &result.positions.len());
-    println!("texcoords: {}", if let Some(texcoords) = &result.texcoords { texcoords.len() } else { 0 });
-    println!("normals: {}", if let Some(normals) = &result.normals { normals.len() } else { 0 });
-    println!("indices: {}", &result.indices.len());
-
-    result.export("res/exported.obj")?;
 
     Ok(())
 }
